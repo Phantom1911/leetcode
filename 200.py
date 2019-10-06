8fe9092f9b33ab80c1a5f9c2c4d50521be3e53c3
Aastik Koshta <aastik@licious.com>
2019-10-07 03:36:01 +0530

    number of islands

---
+++ 200.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def dfs(grid, x, y):
+            if x>=0 and x<len(grid) and y>=0 and y<len(grid[0]) and grid[x][y] != '0':
+                grid[x][y] = '0'
+                for i,j in [[-1,0],[0,1],[1,0],[0,-1]]:
+                    dfs(grid, x+i, y+j)
+        ans = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    dfs(grid, i,j)
+                    ans +=1
+        return ans

5137a0c7ebe9df3710a7da3c550513b38ebffed5
Aastik Koshta <aastik@licious.com>
2019-12-30 19:12:42 +0530

    powerset

---
+++ algoexpert/powerset.py
@@ -0,0 +1,15 @@
+def powerset(array):
+    # Write your code here.
+    ans = [[]]
+    for ele in array:
+        n = len(ans)
+        for i in range(n):
+            arr = ans[i]
+            temp = arr[::]
+            temp.append(ele)
+            ans.append(temp)
+    return ans
+
+
+if __name__=="__main__":
+    print (powerset([1,2,3]))
